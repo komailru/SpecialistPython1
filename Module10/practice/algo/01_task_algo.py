1a81a3b1c038f476cd0b187e0b2e49e41d670f35
komailru <96446963+komailru@users.noreply.github.com>
2021-12-21 15:37:49 +0300

    Update 01_task_algo.py

--- Module10/practice/algo/01_task_algo.py
+++ Module10/practice/algo/01_task_algo.py
@@ -4,3 +4,14 @@
 # Вводится число N, далее еще N − 1 чисел: номера оставшихся карточек (различные числа от 1 до N).
 # Программа должна вывести номер потерянной карточки.
 
+N = int(input("Введите максимальный номер карточки "))
+
+i = 1
+sum_cards_numbers = 0
+sum_all = N
+while i <= N-1:
+    new_number = int(input("Введите номер карточки "))
+    sum_cards_numbers = sum_cards_numbers + new_number
+    sum_all += i
+    i += 1
+print("Потеряли",sum_all - sum_cards_numbers)
