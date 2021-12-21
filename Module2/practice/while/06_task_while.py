5081224bafeeb63e7a86d64640b3c0e0bf262761
komailru <96446963+komailru@users.noreply.github.com>
2021-12-21 15:20:05 +0300

    Update 06_task_while.py

--- Module2/practice/while/06_task_while.py
+++ Module2/practice/while/06_task_while.py
@@ -17,3 +17,27 @@
 # 284 220
 
 # TODO: your code here
+
+N = int(input("Введите целое число "))
+
+first_number = 2
+
+while first_number <= N:
+    i = 1
+    sum1 = 0
+    while i < first_number:
+        if first_number % i == 0:
+            sum1 += i
+        i += 1
+    if 1 < sum1 <= N:
+        #print(sum1)
+        i = 1
+        second_number = sum1
+        sum2 = 0
+        while i < sum1:
+            if sum1 % i == 0:
+                sum2 += i
+            i += 1
+        if first_number == sum2 and first_number != second_number:
+            print(first_number," ",second_number)
+    first_number += 1
