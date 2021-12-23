0a2a7769138cc7befddf5734e3a3b70577444a10
komailru <96446963+komailru@users.noreply.github.com>
2021-12-23 11:43:54 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -26,20 +26,43 @@ staff = [
         'salary': 47800
     },
 ]
+
+max = {'name':staff[0]["name"],'surname':staff[0]["surname"],'salary':staff[0]["salary"]}
+min = {'name':staff[0]["name"],'surname':staff[0]["surname"],'salary':staff[0]["salary"]}
+uniq_surname = {}
+summ = 0
+names = []
+
+for el in staff:
+    if el["salary"] > max["salary"]:
+        max = el
+    if el["salary"] < min["salary"]:
+        min = el
+    summ = el["salary"]
+    names.append(el["surname"])
+    uniq_surname[el["surname"]] = 0
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
-
+print(max)
 # TODO: your code here
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
+print(min)
 
 # TODO: your code here
-
 print("Среднеарифметическую зарплату всех сотрудников")
+print(summ/len(staff))
 
 # TODO: your code here
+number_sun = 0
+for surname in uniq_surname:
+    if names.count(surname) > 1:
+        number_sun += names.count(surname)
+    uniq_surname[surname] = names.count(surname)
 
 print("Количество однофамильцев в организации")
+print(number_sun)
+print(uniq_surname)
 
 # TODO: your code here
 
