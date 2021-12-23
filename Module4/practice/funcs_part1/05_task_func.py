8979014fbbc3223954471424efc6158547d6383d
komailru <96446963+komailru@users.noreply.github.com>
2021-12-23 14:44:21 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -5,5 +5,29 @@
 
 # TODO: your code here
 
+# Не забудьте протестировать вашу функцию
+
+def can_triangle(p1, p2, p3):
+    # TODO: your code here
+    return (p1[0] == p2[0] and p2[0] == p3[0] or p1[1] == p2[1] and p2[1] == p3[1]) != True
+
+def distance(p1, p2):
+    # TODO: your code here
+    pass
+    return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**.5
+
+def perimetr(p1, p2, p3):
+    return distance(p1, p2) + distance(p2, p3) + distance(p1, p3)
+
+def square(p1, p2, p3):
+    p_2 = perimetr(p1, p2, p3) / 2
+    return (p_2*(p_2-distance(p1, p2))*(p_2-distance(p2, p3))*(p_2-distance(p1, p3)))**.5
+
+# Пример вызова функции
+if can_triangle((10, 12), (14, 18), (12, 12)):
+    print("Периметр =",perimetr((10, 12), (14, 18), (12, 12)))
+    print("Площадь =",square((10, 12), (14, 18), (12, 12)))
+else:
+    print("Не треугольник")
 
 # Не забудьте протестировать вашу функцию
