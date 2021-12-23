8f22f356bfd63d4e9cbc89204138534637c38353
komailru <96446963+komailru@users.noreply.github.com>
2021-12-23 14:29:46 +0300

    Update 04_task_func.py

--- Module4/practice/funcs_part1/04_task_func.py
+++ Module4/practice/funcs_part1/04_task_func.py
@@ -3,10 +3,10 @@
 
 def can_triangle(p1, p2, p3):
     # TODO: your code here
-    pass
+    return (p1[0] == p2[0] and p2[0] == p3[0] or p1[1] == p2[1] and p2[1] == p3[1]) != True
 
 
 # Пример вызова функции
-can_triangle((10, 12), (14, 18), (12, 12))
+print(can_triangle((10, 12), (14, 18), (12, 12)))
 
 # Не забудьте протестировать вашу функцию
