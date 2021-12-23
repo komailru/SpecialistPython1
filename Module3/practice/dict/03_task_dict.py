0e627ad6050fe522bc2928465a76ec8a80f96470
komailru <96446963+komailru@users.noreply.github.com>
2021-12-23 12:22:16 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -26,23 +26,57 @@ staff = [
         'salary': 47800
     },
 ]
+
+max = {'name':staff[0]["name"],'surname':staff[0]["surname"],'salary':staff[0]["salary"]}
+min = {'name':staff[0]["name"],'surname':staff[0]["surname"],'salary':staff[0]["salary"]}
+uniq_surname = {}
+summ = 0
+names = []
+uniq_salary = {}
+
+for el in staff:
+    if el["salary"] > max["salary"]:
+        max = el
+    if el["salary"] < min["salary"]:
+        min = el
+    summ = el["salary"]
+    names.append(el["surname"])
+    uniq_surname[el["surname"]] = 0
+    uniq_salary[el["salary"]] = 0
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
-
+print(max)
 # TODO: your code here
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
+print(min)
 
 # TODO: your code here
-
 print("Среднеарифметическую зарплату всех сотрудников")
+print(summ/len(staff))
 
 # TODO: your code here
+number_sun = 0
+for surname in uniq_surname:
+    if names.count(surname) > 1:
+        number_sun += names.count(surname)
+    uniq_surname[surname] = names.count(surname)
 
 print("Количество однофамильцев в организации")
+print(number_sun)
+print(uniq_surname)
 
 # TODO: your code here
-
+sort_salary = []
+for summ in uniq_salary:
+    sort_salary.append(summ)
+#print(sort_salary)
+sort_salary.sort()
+#print(sort_salary)
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
+for summ in sort_salary:
+    for el in staff:
+        if el["salary"] == summ:
+            print(el["name"],el["surname"])
 
 # TODO: your code here
