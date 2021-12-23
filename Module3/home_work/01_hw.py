525eb9e01bd21e2d3b3bba3180c993d202f272d6
komailru <96446963+komailru@users.noreply.github.com>
2021-12-23 07:52:53 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -7,3 +7,11 @@ names = ["Иван", "Ирина", "Вячеслав", "Василий", "Пет
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
+
+text = ""
+for name in names:
+	text = text + name + ", "
+length = len(text)
+if length > 0:	
+	text = text[:length-2]
+print(text)
