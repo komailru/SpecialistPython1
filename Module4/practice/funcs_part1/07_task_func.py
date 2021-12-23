12c786d660383127786deadcfce59452c64316af
komailru <96446963+komailru@users.noreply.github.com>
2021-12-23 15:04:24 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,11 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def my_time(second):
+    hours = second // 3600
+    minutes = (second - hours * 3600) // 60
+    seconds = second - hours * 3600 - minutes * 60
+    return f"{hours:0>2}:{minutes:0>2}:{seconds:0>2}"
+
+print(my_time(29143))
