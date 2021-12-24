0c83c22fca5700bddc9071c84badb90845aa7d73
komailru <96446963+komailru@users.noreply.github.com>
2021-12-24 12:05:35 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -6,14 +6,23 @@
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется ровно одной пустой строкой от предыдущего и после последнего нет пустой строки
 
-path = "dir/limericks.txt"  # вместо dir подставь название папки с файлом.
+path = "data_files/limerics.txt"  # вместо dir подставь название папки с файлом.
 # Или удалите dir, если limericks.txt в тойже папке что и текущий файл
 
 # Открываем файл на чтение
-f = open(path, "r")
+f = open(path, "r", encoding="utf-8")
+number_symblos = 0
+number_poems   = 0
 # В переменную line считываем строку за стройкой из файла(f)
 for line in f:
-    ...
+    print(line.rstrip())
+    number_symblos = number_symblos + len(line.strip())
+    if len(line) == 1:
+        number_poems += 1
+if number_symblos > 0:
+    number_poems += 1
+print(number_symblos)
+print(number_poems)
 
 # Подсказка: пустые строки выглядят так "\n". Помните? Строка считывается вместе с символом переноса!
 # Применение метода "\n".rstrip() --> "" вернет вам пустую строку, строку из НУЛЯ символов.
