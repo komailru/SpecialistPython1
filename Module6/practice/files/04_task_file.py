277542a9f958f36047135ed09bc4c12ce181f5a9
komailru <96446963+komailru@users.noreply.github.com>
2021-12-24 13:09:23 +0300

    Update 04_task_file.py

--- Module6/practice/files/04_task_file.py
+++ Module6/practice/files/04_task_file.py
@@ -11,3 +11,25 @@
 # Возможно пригодится:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+my_path = "data_files/"
+name_in = "fruits.txt"
+
+output = {}
+
+f = open(my_path+name_in, "r", encoding="utf-8")
+for line in f:
+    if len(line) == 1:
+        continue
+    First_letter = line[:1]
+    if output.get(First_letter) == None:
+        output[First_letter] = []
+    output[First_letter].append(line)
+f.close()
+print(output)
+
+for el in output:
+    f = open(my_path+"fruits_"+el, "w", encoding="utf-8")
+    for item in output[el]:
+        f.write(item)
+    f.close()
